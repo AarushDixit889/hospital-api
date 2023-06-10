9fb438a1bc032116d9bdf36fb4591691a993dadb
Aarush Dixit <133681987+AarushDixit889@users.noreply.github.com>
2023-06-10 18:59:22 +0530

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,1 +1,40 @@
-
+from fastapi import FastAPI
+from pickle import load
+from pandas import read_csv
+import numpy as np
+app=FastAPI()
+@app.post("/cancer")
+async def create_upload_file(filepath):
+    cancer_df=read_csv(filepath)
+    cancer=load("../models/cancer.pkl")
+    prediction=np.array(cancer.predict(cancer_df))
+    preds=np.where(prediction==1,"Yes","No")
+    return preds
+@app.post("/diabeties")
+async def create_upload_file(filepath):
+    cancer_df=read_csv(filepath)
+    cancer=load("../models/diabeties.pkl")
+    prediction=np.array(cancer.predict(cancer_df))
+    preds=np.where(prediction==1,"Yes","No")
+    return preds
+@app.post("/heart")
+async def create_upload_file(filepath):
+    cancer_df=read_csv(filepath)
+    cancer=load("../models/heart.pkl")
+    prediction=np.array(cancer.predict(cancer_df))
+    preds=np.where(prediction==1,"Yes","No")
+    return preds
+@app.post("/kidney")
+async def create_upload_file(filepath):
+    cancer_df=read_csv(filepath)
+    cancer=load("../models/kidney.pkl")
+    prediction=np.array(cancer.predict(cancer_df))
+    preds=np.where(prediction==1,"Yes","No")
+    return preds
+@app.post("/liver")
+async def create_upload_file(filepath):
+    cancer_df=read_csv(filepath)
+    cancer=load("../models/liver.pkl")
+    prediction=np.array(cancer.predict(cancer_df))
+    preds=np.where(prediction==1,"Yes","No")
+    return preds
